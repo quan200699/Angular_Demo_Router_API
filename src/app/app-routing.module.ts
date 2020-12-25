71bba3f5e7cb04b02f064fb8190c3b6c5e1616ba
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-12-25 10:01:35 +0700

    add routing for category

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {CreateProductComponent} from './product/create-product/create-product.component';
-import {ListProductComponent} from './product/list-product/list-product.component';
-import {UpdateProductComponent} from './product/update-product/update-product.component';
-import {DeleteProductComponent} from './product/delete-product/delete-product.component';
 
 
 const routes: Routes = [
   {
     path: 'products',
     loadChildren: () => import('./product/product.module').then(module => module.ProductModule)
+  },
+  {
+    path: 'categories',
+    loadChildren: () => import('./category/category.module').then(module => module.CategoryModule)
   }
 ];
 
