4498fb4f862f5c41489f657e13b01b5c0939bc34
quan200699 <51079189+quan200699@users.noreply.github.com>
2021-01-04 15:11:41 +0700

    crud 2 bang

--- src/app/product/create-product/create-product.component.ts
+++ src/app/product/create-product/create-product.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Product} from '../../model/product';
 import {ProductService} from '../../service/product/product.service';
+import {Category} from '../../model/category';
+import {CategoryService} from '../../service/category/category.service';
 
 @Component({
   selector: 'app-create-product',
@@ -8,12 +10,20 @@ import {ProductService} from '../../service/product/product.service';
   styleUrls: ['./create-product.component.css']
 })
 export class CreateProductComponent implements OnInit {
-  product: Product = {};
+  product: Product = {
+    category: {
+      id: -1
+    }
+  };
+  categoryList: Category[] = [];
 
-  constructor(private productService: ProductService) {
+  constructor(private productService: ProductService,
+              private categoryService: CategoryService) {
+
   }
 
   ngOnInit() {
+    this.getAllCategory();
   }
 
   createProduct() {
@@ -21,6 +31,12 @@ export class CreateProductComponent implements OnInit {
       console.log('Thành công');
     }, () => {
       console.log('Xảy ra lỗi');
+    });
+  }
+
+  getAllCategory() {
+    this.categoryService.getAllCategory().subscribe(categoryList => {
+      this.categoryList = categoryList;
     });
   }
 }
